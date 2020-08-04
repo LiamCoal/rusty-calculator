47cb6694b6b9e34957040f6bca4141650035101b
lim <liam@liamiam.com>
2020-08-03 17:15:45 -0700

    Use function pointers instead of repeating.

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,17 @@ fn main() {
     if args_s.len() > 3 {
         let val1 = str::parse::<f64>(args[2]).unwrap();
         let val2 = str::parse::<f64>(args[3]).unwrap();
-        match args[1] {
-            "+" | "add" => {
-                print_math(val1, val2, val1 + val2, "+");
-            }
-            "-" | "sub" => {
-                print_math(val1, val2, val1 - val2, "-");
-            }
-            "*" | "mul" => {
-                print_math(val1, val2, val1 * val2, "*");
-            }
-            "/" | "div" => {
-                print_math(val1, val2, val1 / val2, "/");
-            }
+        let thing: (&str, fn(f64, f64) -> f64) = match args[1] {
+            "+" | "add" => ("+", add),
+            "-" | "sub" => ("-", sub),
+            "/" | "div" => ("/", mul),
+            "*" | "mul" => ("*", div),
             _ => {
                 writeln!(std::io::stderr(), "Usage: {} <+, -, *, /, --version> <arg1> <arg2>", args[0]).unwrap();
+                return;
             }
-        }
+        };
+        print_math(val1, val2, (thing.1)(val1, val2), thing.0);
     } else if args_s.len() > 1 {
         if args[1] == "--version" {
             println!("{} {}", args[0].bright_green(), version.bright_blue());
@@ -52,3 +46,8 @@ fn print_math(first_argument: f64,
              "=".white(),
              result.to_string().as_str().green().bold());
 }
+
+fn add(arg1: f64, arg2: f64) -> f64 { arg1 + arg2 }
+fn sub(arg1: f64, arg2: f64) -> f64 { arg1 - arg2 }
+fn mul(arg1: f64, arg2: f64) -> f64 { arg1 * arg2 }
+fn div(arg1: f64, arg2: f64) -> f64 { arg1 / arg2 }
