f792d96e00f785e324341568957ff65318f7755c
lim <liam@liamiam.com>
2020-08-07 16:33:39 -0700

    colorrrrrrrs

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,18 @@ extern crate colored;
 use crate::calculator::calculate;
 use std::collections::HashMap;
 use std::io::{stdin, stdout, Write};
+use colored::Colorize;
 
 fn main() {
     let variables: &mut HashMap<String, f64> = &mut HashMap::new();
     if std::env::args().len() == 1 {
         loop {
-            print!("<= : ");
+            print!("{}", "<= : ".white());
             stdout().flush().expect("Unable to flush output.");
             let mut exp = String::new();
             stdin().read_line(&mut exp).expect("Error.");
-            println!("=>   {}", calculate(&*exp, variables));
+            print!("{}", "=>   ".white());
+            println!("{}", (&*calculate(&*exp, variables).to_string()).bright_green().bold());
         }
     } else {
         for arg in std::env::args().skip(1) {
