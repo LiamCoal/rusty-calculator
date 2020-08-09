abb45de4b47b60c9de610fb6892307926cd5243c
lim <liam@liamiam.com>
2020-08-09 14:55:16 -0700

    Better command line support

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,41 @@ use crate::calculator::calculate;
 use std::collections::HashMap;
 use std::io::{stdin, stdout, Write};
 use colored::Colorize;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() {
     let variables: &mut HashMap<String, f64> = &mut HashMap::new();
     if std::env::args().len() == 1 {
-        loop {
-            print!("{}", "<= : ".white());
-            stdout().flush().expect("Unable to flush output.");
-            let mut exp = String::new();
-            stdin().read_line(&mut exp).expect("Error.");
-            print!("{}", "=>   ".white());
-            println!("{}", (&*calculate(&*exp, variables).to_string()).bright_green().bold());
+        let mut rl = Editor::<()>::new();
+        if rl.load_history("history.txt").is_err() {
+            println!("No expression history found.");
         }
+        loop {
+            let mut exp = String::new();
+            let readline = rl.readline(&*("<= : "));
+            match readline {
+                Ok(line) => {
+                    rl.add_history_entry(line.as_str());
+                    exp = line;
+                },
+                Err(ReadlineError::Interrupted) => {
+                    println!("CTRL-C");
+                    break
+                },
+                Err(ReadlineError::Eof) => {
+                    println!("CTRL-D");
+                    break
+                },
+                Err(err) => {
+                    println!("Error: {:?}", err);
+                    break
+                }
+            }
+            print!("{}", "=>   ");
+            println!("{}", (&*calculate(&*exp, variables).to_string()).green().bold());
+        }
+        rl.save_history("history.txt").unwrap();
     } else {
         for arg in std::env::args().skip(1) {
             println!("=>   {}", calculate(&*arg, variables));
