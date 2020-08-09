a49cd7e3ecc575725b7d1139d23821bc869f69e0
lim <liam@liamiam.com>
2020-08-09 15:07:27 -0700

    Add scripting support

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ extern crate colored;
 
 use crate::calculator::calculate;
 use std::collections::HashMap;
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Write, Read};
 use colored::Colorize;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use std::fs::File;
+use std::env::args;
+use std::borrow::Borrow;
 
 fn main() {
     let variables: &mut HashMap<String, f64> = &mut HashMap::new();
+    let mut args = args().collect::<Vec<String>>();
+    let argsv = &*args.clone();
     if std::env::args().len() == 1 {
         let mut rl = Editor::<()>::new();
         if rl.load_history("history.txt").is_err() {
@@ -42,6 +47,13 @@ fn main() {
             println!("{}", (&*calculate(&*exp, variables).to_string()).green().bold());
         }
         rl.save_history("history.txt").unwrap();
+    } else if File::open(argsv.get(1).unwrap()).is_ok() {
+        let mut script = File::open(argsv.get(1).unwrap()).unwrap();
+        let mut file = String::new();
+        script.read_to_string(&mut file);
+        for line in file.split("\n") {
+            println!("{} => {}", line.italic(), (&*calculate(line, variables).to_string()).green().bold())
+        }
     } else {
         for arg in std::env::args().skip(1) {
             println!("=>   {}", calculate(&*arg, variables));
